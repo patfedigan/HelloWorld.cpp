0d9a31661332180015ecffe7de24d2dead80d54f
pfedigan <pfederation@gmail.com>
2014-10-18 13:44:00 -0700

    init commit
    the most basic of cpp programs

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/* My first C++ program
+   Author: Patrick Fedigan
+   Date: 8/21/2013 */
+   
+#include <iostream>
+
+int main() {
+	
+	std::cout << "Hello, World!\n\n";
+	
+	std::cout << "Press Enter to Continue";
+	
+	std::cin.get();
+	
+	return 0;
+	
+}
